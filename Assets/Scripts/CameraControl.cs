cf07bdc922f892fd23e2c4df86e0478169464418
Jordan Kniest <mail@jkniest.de>
2020-04-18 12:59:12 +0200

    Fix terrain position, zoom with rotation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Timeline;
 
 public class CameraControl : MonoBehaviour
 {
     private Vector2 _velocity = Vector2.zero;
     private Vector2 _mouseVelocity = Vector2.zero;
 
-    private float _zoomVelocity = 0;
-    private Camera _camera;
-
     private const float MouseOffset = 200;
-
-    private void Start()
-    {
-        _camera = GetComponent<Camera>();
-    }
     
     public void Move(InputAction.CallbackContext context)
     {
@@ -31,22 +19,10 @@ public class CameraControl : MonoBehaviour
         _velocity = context.ReadValue<Vector2>();
     }
 
-    public void Zoom(InputAction.CallbackContext context)
-    {
-        if (context.canceled)
-        {
-            _zoomVelocity = 0;
-            return;
-        }
-
-        var value = context.ReadValue<float>();
-
-        _zoomVelocity = value < 0 ? -1 : value > 0 ? 1 : 0;
-    }
-
     private void Update()
     {
         _mouseVelocity = Vector2.zero;
+        return;
 
         var mouse = Input.mousePosition;
 
@@ -81,9 +57,7 @@ public class CameraControl : MonoBehaviour
             position + new Vector3(velocity.x, 0, velocity.y) * 40,
             Time.fixedDeltaTime
         );
-        
-        transform.position = position;
 
-        _camera.fieldOfView = Mathf.Clamp(_camera.fieldOfView + _zoomVelocity, 30, 100);
+        transform.position = position;
     }
 }
---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraZoom : MonoBehaviour
+{
+    private float _velocity = 0f;
+    private float _zoomLevel = 0;
+
+    private const float Speed = 20f;
+
+    public void Zoom(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            _velocity = 0;
+            return;
+        }
+
+        var value = context.ReadValue<float>();
+
+        _velocity = value < 0 ? -1 : value > 0 ? 1 : 0;
+    }
+
+    private void FixedUpdate()
+    {
+        var max = 35f / 20f * Speed;
+
+        _zoomLevel = Mathf.Clamp(_zoomLevel + _velocity, -10f / 20f * Speed, max);
+
+        var transform1 = transform;
+
+        var position = transform1.forward * _zoomLevel * Speed * Time.fixedDeltaTime;
+        transform1.localPosition = position;
+
+        // level / max
+        transform.localRotation = Quaternion.Euler(
+            Mathf.Lerp(60, 45, _zoomLevel / max),
+            0,
+            0
+        );
+    }
+}
